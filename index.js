8c8a2f868c0f98d1c70a41d7c7d8ee3faf6f8609
Sarah <34101308+sarahfoi@users.noreply.github.com>
2020-06-18 16:52:10 -0300

    Início

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+/*-------------ROTAS-------------*/
+
+//HOME
+app.get('/', (req, res)=>{
+    res.render("index"); 
+});
+
+/*-----------FIM ROTAS-----------*/
+app.listen(8000, ()=>{
+    console.log("Site rodando");
+});
